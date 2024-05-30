3d21ce054dad2f7a15a06dc32b917928910c9947
Jamie Li <lijiachi03@gmail.com>
2024-05-30 14:26:41 -0400

    update getSQLDumpRequest api

--- frontend/src/api/ExecutionRequestApi.tsx
+++ frontend/src/api/ExecutionRequestApi.tsx
@@ -312,8 +312,10 @@ const addReviewToRequest = async (
   );
 };
 
-const getSQLDumpRequest = async (connectionId: number) => {
-  console.log("-------getSQLDumpRequest---------")
+const getSQLDumpRequest = async (connectionId: string) => {
+  console.log("-------getting SQL Dump---------")
+  console.log("connectionId: ", connectionId)
+  
   const response = await fetch(`${requestUrl}sql-dump/${connectionId}`, {
     method: "GET",
     headers: {
@@ -321,9 +323,24 @@ const getSQLDumpRequest = async (connectionId: number) => {
     },
     credentials: "include",
   });
-  // TODO: it returns .sql not json
-  await response.json();
-  return;
+
+  if (response.ok) {
+    // Create a Blob from the response
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `dump-${connectionId}.sql`; // Set the filename
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    window.URL.revokeObjectURL(url);
+
+  } else {
+    console.error('Failed to download SQL dump');
+  }
 };
 
 function withType<T, U extends string>(schema: z.ZodSchema<T>, typeValue: U) {
