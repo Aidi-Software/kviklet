68bccbb1edad2e7ba40e6f2191b5140851268bfa
Jamie Li <lijiachi03@gmail.com>
2024-06-12 12:45:59 -0400

    update RawDatasourceRequestSchema

--- frontend/src/api/ExecutionRequestApi.tsx
+++ frontend/src/api/ExecutionRequestApi.tsx
@@ -81,7 +81,7 @@ const ExecuteEvent = withType(
 
 const RawDatasourceRequestSchema = z.object({
   id: z.string(),
-  type: z.enum(["TemporaryAccess", "SingleExecution"]),
+  type: z.enum(["TemporaryAccess", "SingleExecution", "GetSQLDump"]),
   author: userResponseSchema,
   title: z.string().min(1),
   description: z.string(),
